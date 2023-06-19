280c126252fc82e354d456e7d487593cf8904ed3
Konulsi <konulsi@code.edu.az>
2023-06-19 10:43:02 +0400

    initial

---
+++ CallaApp/CallaApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CallaApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CallaApp/CallaApp/Controllers/WishlistController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CallaApp.Controllers
+{
+    public class WishlistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
